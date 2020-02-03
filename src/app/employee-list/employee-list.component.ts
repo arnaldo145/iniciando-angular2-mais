3c248340a7a721412396fc4a31f94c366d670b5c
Arnaldo Madeira <arnaldomadeira11@hotmail.com>
2020-02-02 21:17:48 -0300

    Iniciando projeto do curso de Angular 2+ - School Of Net

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import employees from '../employees';
+
+@Component({
+  selector: 'employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  employees = employees;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/employee-new/employee-new.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import employees from '../employees';
+
+@Component({
+  selector: 'employee-new',
+  templateUrl: './employee-new.component.html',
+  styleUrls: ['./employee-new.component.css']
+})
+export class EmployeeNewComponent implements OnInit {
+
+  name: string = '';
+  salary: number = 0;
+  employees = employees;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addEmployee(){
+    this.employees.push({name: this.name, salary: this.salary});
+    console.log(this.employees);
+  }
+
+}
